6611148db2042b1e3a0864da2d5b03771640105d
Dmitriy <dmitriy@example.com>
2026-06-16 19:14:04 +0500

    refactor(storage): reorganize storage services

---
+++ src/app/core/services/index.ts
@@ -0,0 +1,3 @@
+export * from "@core/services/storage-entity.service";
+export * from "@core/services/storage-cache.service";
+export * from "@core/services/unified-storage.service";
--- src/app/services/storage/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "@services/storage/storage-entity.service";
-export * from "@services/storage/storage-cache.service";
-export * from "@services/storage/unified-storage.service";
